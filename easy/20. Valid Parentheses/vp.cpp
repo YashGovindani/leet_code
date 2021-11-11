8ff6af46340ab7e1b2956ef19511d311098f82d9
YashGovindani <yashgovindani222@gmail.com>
2021-11-11 16:40:47 +0530

    question 20

--- easy/20. Valid Parentheses/vp.cpp
+++ easy/20. Valid Parentheses/vp.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> charStack;
+        map<char, char> m { {')','('}, {'}', '{'}, {']', '['}};
         for(char c : s)
         {
             if(charStack.empty() || c == '(' || c == '{' || c == '[') charStack.push(c);
-            else if(c == ')')
-            {
-                if(charStack.top() != '(') return false;
-                charStack.pop();
-            }
-            else if(c == ']')
-            {
-                if(charStack.top() != '[') return false;
-                charStack.pop();
-            }
             else
             {
-                if(charStack.top() != '{') return false;
-                charStack.pop();
+                if(m[c] != charStack.top()) return false;
+                else charStack.pop();
             }
         }
         return charStack.empty();
